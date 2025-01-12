890b0bb1d703875c7e33781d509e2ecb100a964e
zfourati <zeineb.fourati@epfl.ch>
2025-01-12 18:48:04 +0100

    Create baseline hypercolumn

---
+++ Baseline_Hypercolumn.py
@@ -0,0 +1,141 @@
+import torch
+import rasterio
+from torch.utils.data import Dataset
+from torch.utils.data import ConcatDataset, random_split, DataLoader
+from PIL import Image
+import os
+import rasterio
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from collections import Counter
+from tqdm.notebook import trange
+import random
+from torch.optim import SGD
+import glob
+from Function_lib import *
+
+print('GPU available: ',torch.cuda.is_available())
+seed = 323444           # the seed value used to initialise the random number generator of PyTorch
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+
+path_to_model = 'cnn_states/HypercolumnBaseline'
+os.makedirs(path_to_model, exist_ok=True)
+
+path_to_plot = 'Plots/Baseline'
+os.makedirs(path_to_plot, exist_ok=True)
+
+# define hyperparameters
+device = 'cuda'
+start_epoch = 'latest' # set to 0 to start from scratch again or to 'latest' to continue training from saved checkpoint
+batch_size = 30
+learning_rate = 0.1
+weight_decay = 0.001
+num_epochs = 10
+validation_split_ratio = 0.2
+
+label_names = [
+    "Bad data",
+    "Snow and Ice",
+    "Wet ice and meltwater",
+    "Freshwater",
+    "Sediment",
+    "Bedrock",
+    "Vegetation",
+    ]
+
+class Hypercolumn(nn.Module):
+
+    def __init__(self):
+        super(Hypercolumn, self).__init__()
+
+        #TODO: define your architecture and forward pass here
+        self.block1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=5, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(inplace=True)
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(inplace=True)
+        )
+        self.block3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(inplace=True)
+        )
+        self.block4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(inplace=True)
+        )
+        self.final = nn.Sequential(
+            nn.Conv2d(483, 256, kernel_size=1, stride=1),           # 3 (input) + 32 + 64 + 128 + 256 = 487
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 7, kernel_size=1, stride=1)
+        )
+
+
+    def forward(self, x):
+        #TODO
+        upsample = nn.Upsample(size=(x.size(2), x.size(3)))
+        x1 = self.block1(x)
+        x2 = self.block2(x1)
+        x3 = self.block3(x2)
+        x4 = self.block4(x3)
+
+        hypercol = torch.cat(
+            (x, upsample(x1), upsample(x2), upsample(x3), upsample(x4)),
+            dim=1)
+        return self.final(hypercol)
+    
+criterion = nn.CrossEntropyLoss()
+
+dl_train = LoadData(batch_size, split='train', num_workers=1)
+dl_val = LoadData(batch_size, split='val', num_workers=1)
+
+# load model
+model, epoch = load_model(Hypercolumn(), path_to_model, epoch=start_epoch)
+optim = setup_optimiser(model, learning_rate, weight_decay)
+
+# do epochs
+while epoch < num_epochs:
+
+    # training
+    model, loss_train, oa_train = train_epoch(dl_train, model, optim, device)
+
+    # validation
+    loss_val, oa_val = validate_epoch(dl_val, model, device)
+
+    # print stats
+    print('[Ep. {}/{}] Loss train: {:.2f}, val: {:.2f}; OA train: {:.2f}, val: {:.2f}'.format(
+        epoch+1, num_epochs,
+        loss_train, loss_val,
+        100*oa_train, 100*oa_val
+    ))
+
+    # save model
+    epoch += 1
+    save_model(model, epoch, path_to_model)
+    
+    
+#Testing
+dl_test = LoadData(batch_size, split='test', num_workers=1)
+loss_test, oa_test = validate_epoch(dl_test, model, device)
+print('Testing:  Loss: {:.2f}  OA: {:.2f}'.format(loss_test, 100*oa_test))
+
+#Visualize predictions and label class distribution
+dl_test_single = LoadData(batch_size=1, split='test', num_workers=1)                
+visualize(dl_test_single,model, path_to_plot, path_to_model)
+
+dl_train_single = LoadData(batch_size, split='train', num_workers=1)
+dl_val_single = LoadData(batch_size, split='val', num_workers=1)
+plot_label_distribution(dl_train_single, path_to_plot)
+plot_label_distribution(dl_train_single,path_to_plot,  state = 'val')
